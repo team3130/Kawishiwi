6e6da07d1110cabd092636b6157a67a07e6c3cb6
Tyler-Bibus <57819543+Tyler-Bibus@users.noreply.github.com>
2020-01-10 17:06:29 -0600

    Revert "Commented out wheel of fortune color sensor code for testing"
    This reverts commit 97461f3f119992dee63be0726ff911db470fe0b8.

--- src/main/java/frc/team3130/robot/subsystems/WheelOfFortune.java
+++ src/main/java/frc/team3130/robot/subsystems/WheelOfFortune.java
@@ -12,101 +12,88 @@ import frc.team3130.robot.RobotMap;
 import frc.team3130.robot.commands.SpinWheel;
 
 public class WheelOfFortune implements Subsystem {
+
+    //Create necessary objects
+    private static ColorSensorV3 m_colorSensor;
+    private static ColorMatch m_colorMatcher;
+
+    //Create and define all standard data types needed
+    private final I2C.Port i2cPort = I2C.Port.kOnboard;
+
+    private static final Color kCyanTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+    private static final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+    private static final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+    private static final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
     /**
-     * //Create necessary objects
-     * private static ColorSensorV3 m_colorSensor;
-     * private static ColorMatch m_colorMatcher;
-     * <p>
-     * //Create and define all standard data types needed
-     * private final I2C.Port i2cPort = I2C.Port.kOnboard;
-     * <p>
-     * private static final Color kCyanTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-     * private static final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-     * private static final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-     * private static final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
-     * /**
      * The Singleton instance of this WheelOfFortune. External classes should
      * use the {@link #getInstance()} method to get the instance.
-     * <p>
-     * private final static WheelOfFortune INSTANCE = new WheelOfFortune();
-     * <p>
-     * /**
+     */
+    private final static WheelOfFortune INSTANCE = new WheelOfFortune();
+
+    /**
      * Returns the Singleton instance of this WheelOfFortune. This static method
      * should be used -- {@code WheelOfFortune.getInstance();} -- by external
      * classes, rather than the constructor to get the instance of this class.
-     * <p>
-     * public static WheelOfFortune getInstance() {
-     * return INSTANCE;
-     * }
-     * <p>
-     * private WheelOfFortune() {
-     * m_colorSensor = new ColorSensorV3(i2cPort);
-     * m_colorMatcher = new ColorMatch();
-     * <p>
-     * m_colorMatcher.addColorMatch(kCyanTarget);
-     * m_colorMatcher.addColorMatch(kGreenTarget);
-     * m_colorMatcher.addColorMatch(kRedTarget);
-     * m_colorMatcher.addColorMatch(kYellowTarget);
-     * }
-     * <p>
-     * public static int proximityCheck() {
-     * return m_colorSensor.getProximity();
-     * }
-     * <p>
-     * <p>
-     * /**
-     * Run the color match algorithm on our detected color
-     *
-     * @return String name of the most likely color
-     * <p>
-     * public static String detectColor() {
-     * Color detectedColor = m_colorSensor.getColor();
-     * <p>
-     * String colorString;
-     * ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
-     * <p>
-     * if (match.color == kCyanTarget) {
-     * colorString = "Cyan";
-     * } else if (match.color == kRedTarget) {
-     * colorString = "Red";
-     * } else if (match.color == kGreenTarget) {
-     * colorString = "Green";
-     * } else if (match.color == kYellowTarget) {
-     * colorString = "Yellow";
-     * } else {
-     * colorString = "Unknown";
-     * }
-     * <p>
-     * SmartDashboard.putNumber("Red", detectedColor.red);
-     * SmartDashboard.putNumber("Green", detectedColor.green);
-     * SmartDashboard.putNumber("Blue", detectedColor.blue);
-     * SmartDashboard.putNumber("Confidence", match.confidence);
-     * SmartDashboard.putString("Detected Color", colorString);
-     * SmartDashboard.putNumber("Proximity", proximityCheck());
-     * <p>
-     * return colorString;
-     * }
      */
-    private static WPI_TalonSRX m_spinWheel;
-
-    private final static WheelOfFortune INSTANCE = new WheelOfFortune();
-
     public static WheelOfFortune getInstance() {
         return INSTANCE;
     }
 
     private WheelOfFortune() {
-        m_spinWheel = new WPI_TalonSRX(RobotMap.CAN_SKYWALKER);
-        m_spinWheel.configFactoryDefault();
+        m_colorSensor = new ColorSensorV3(i2cPort);
+        m_colorMatcher = new ColorMatch();
+
+        m_colorMatcher.addColorMatch(kCyanTarget);
+        m_colorMatcher.addColorMatch(kGreenTarget);
+        m_colorMatcher.addColorMatch(kRedTarget);
+        m_colorMatcher.addColorMatch(kYellowTarget);
     }
 
+    public static int proximityCheck() {
+        return m_colorSensor.getProximity();
+    }
+
+
+    /**
+     * Run the color match algorithm on our detected color
+     *
+     * @return String name of the most likely color
+     */
+    public static String detectColor() {
+        Color detectedColor = m_colorSensor.getColor();
+
+        String colorString;
+        ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+
+        if (match.color == kCyanTarget) {
+            colorString = "Cyan";
+        } else if (match.color == kRedTarget) {
+            colorString = "Red";
+        } else if (match.color == kGreenTarget) {
+            colorString = "Green";
+        } else if (match.color == kYellowTarget) {
+            colorString = "Yellow";
+        } else {
+            colorString = "Unknown";
+        }
+
+        SmartDashboard.putNumber("Red", detectedColor.red);
+        SmartDashboard.putNumber("Green", detectedColor.green);
+        SmartDashboard.putNumber("Blue", detectedColor.blue);
+        SmartDashboard.putNumber("Confidence", match.confidence);
+        SmartDashboard.putString("Detected Color", colorString);
+        SmartDashboard.putNumber("Proximity", proximityCheck());
+
+        return colorString;
+    }
+
+    private static WPI_TalonSRX m_spinWheel;
+
+    
 
     public static void motorSpin(double spin) {
-        m_spinWheel.set(spin);
+
     }
-
-
-
 
     public static void outputToSmartDashboard() {
 
