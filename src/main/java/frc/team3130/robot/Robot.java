3a726e5dce9a8b3573e59c34ba0ad3cb13da80c6
ckugel <53205881+ckugel@users.noreply.github.com>
2020-01-11 15:14:18 -0600

    defined more subsytems and commands

--- src/main/java/frc/team3130/robot/Robot.java
+++ src/main/java/frc/team3130/robot/Robot.java
@@ -4,10 +4,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.team3130.robot.commands.DefaultDrive;
-import frc.team3130.robot.commands.IntakeIn;
-import frc.team3130.robot.commands.SkyWalker;
-import frc.team3130.robot.commands.SpinShooter;
+import frc.team3130.robot.commands.*;
 import frc.team3130.robot.subsystems.*;
 
 /**
@@ -36,10 +33,12 @@ public class Robot extends TimedRobot {
         scheduler.setDefaultCommand(Climber.getInstance(), new SkyWalker());
         scheduler.setDefaultCommand(Intake.getInstance(), new IntakeIn());
         scheduler.setDefaultCommand(Shooter.getInstance(), new SpinShooter());
-        scheduler.registerSubsystem(Intake.getInstance());
+        scheduler.setDefaultCommand(WheelOfFortune.getInstance(), new TripleSpinFinish());
+        scheduler.setDefaultCommand(Intake.getInstance(), new IntakeOut());
         scheduler.registerSubsystem(Shooter.getInstance());
         scheduler.registerSubsystem(WheelOfFortune.getInstance());
-
+        scheduler.registerSubsystem(Chassis.getInstance());
+        scheduler.registerSubsystem();
 
 
         //Smartdash output thread
