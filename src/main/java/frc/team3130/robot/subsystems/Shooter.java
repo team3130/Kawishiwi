dc9e55940ddd8c6e70b82f0e175d03b2bd4cb629
Rahul Polavarapu <rahul.polavarapu@gmail.com>
2020-01-22 18:01:47 -0600

    changed shooter motors to falcons
    this can be changed back if need be

--- src/main/java/frc/team3130/robot/subsystems/Shooter.java
+++ src/main/java/frc/team3130/robot/subsystems/Shooter.java
@@ -1,5 +1,6 @@
 package frc.team3130.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.team3130.robot.RobotMap;
@@ -7,9 +8,9 @@ import frc.team3130.robot.RobotMap;
 public class Shooter implements Subsystem {
 
     //Create necessary objects
-    private static WPI_TalonSRX m_shooterLeft;
-    private static WPI_TalonSRX m_shooterRight;
-    private static WPI_TalonSRX m_shooterTop;
+    private static WPI_TalonFX m_shooterLeft;
+    private static WPI_TalonFX m_shooterRight;
+    private static WPI_TalonFX m_shooterTop;
 
 
     //Create and define all standard data types needed
@@ -30,9 +31,9 @@ public class Shooter implements Subsystem {
     }
 
     private Shooter() {
-        m_shooterLeft = new WPI_TalonSRX(RobotMap.CAN_SHOOTERLEFT);
-        m_shooterRight = new WPI_TalonSRX(RobotMap.CAN_SHOOTERRIGHT);
-        m_shooterTop = new WPI_TalonSRX(RobotMap.CAN_SHOOTERTOP);
+        m_shooterLeft = new WPI_TalonFX(RobotMap.CAN_SHOOTERLEFT);
+        m_shooterRight = new WPI_TalonFX(RobotMap.CAN_SHOOTERRIGHT);
+        m_shooterTop = new WPI_TalonFX(RobotMap.CAN_SHOOTERTOP);
 
         m_shooterLeft.configFactoryDefault();
         m_shooterRight.configFactoryDefault();
